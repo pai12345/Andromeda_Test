697609179c2f452106773db2dd461e783134243f
pai12345 <rahulpai123sap@gmail.com>
2022-06-09 10:14:29 +0200

    Updated requirements.txt file

--- router/test.py
+++ router/test.py
@@ -5,12 +5,28 @@ from config.config import get_env
 
 testroute = APIRouter()
 
-# ping test
-@testroute.get('/pingtest')
-def pingtest():
+# self ping test
+@testroute.get('/selfpingtest')
+def selfpingtest():
     try:
         env = get_env()
         pingtest = ping.fetch(env["svc"])
         return asyncio.run(pingtest)
+    except BaseException as error:
+        return error
+
+# ping test
+@testroute.get('/pingtest1')
+def pingtest1():
+    try:
+        return "server 1"
+    except BaseException as error:
+        return error
+
+# ping test
+@testroute.get('/pingtest2')
+def pingtest2():
+    try:
+        return "server 2"
     except BaseException as error:
         return error
